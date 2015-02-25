c4ffea77631c124f7591f6b6820f34b055de886c
Eric Wilson <eric@triadicclosure.com>
2015-02-24 16:25:23 -0800

    added in womens shoe sizing

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
     
+    @IBOutlet weak var womensShoeSizeTextField: UITextField!
+    
+    @IBOutlet weak var womensConvertedShoeSizeLabel: UILabel!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +36,14 @@ class ViewController: UIViewController {
         mensConvertedShoeSizeLabel.hidden = false
         mensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European shoe size"
     }
+    
+    @IBAction func convertWomensShoeSizeButtonPressed(sender: UIButton) {
+        let sizeFromTextField = Double((womensShoeSizeTextField.text as NSString).doubleValue)
+        let conversionConstant = 30.5
+        womensConvertedShoeSizeLabel.hidden = false
+        womensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European shoe size"
+    }
+    
 
 }
 
